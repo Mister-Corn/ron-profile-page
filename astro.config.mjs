8045f973e5ff836eaf3daef54af84b8ec3b8d51b
Ronald Libago <36778342+Mister-Corn@users.noreply.github.com>
2023-08-17 13:36:05 -0700

    Add vite config override to enable refresh
    Look at references in last commit.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,13 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  vite: {
+    server:{
+      host: "0.0.0.0",
+      hmr: { clientPort: 3000 },
+      port: 3000, 
+      watch: { usePolling: true }
+    }
+  }
 });
