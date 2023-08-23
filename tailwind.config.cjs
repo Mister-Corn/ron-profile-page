fec37dfac3f47eb193ad629d54c9305cf790c820
Ronald Libago <me@ronaldlibago.dev>
2023-08-22 20:05:00 -0700

    Add custom colors to Tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,13 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"summerYellow": "rgb(255 255 105 / <alpha-value>)",
+				"pastelGreen": "rgb(105 255 180 / <alpha-value>)",
+				"boldPurple": "rgb(151 67 226 / <alpha-value>)",
+			}
+		},
 	},
 	plugins: [],
 }
