dbfb7e36efa44936bf2451ae8366b403b6d11ef2
Ronald Libago <me@ronaldlibago.dev>
2023-09-14 15:04:23 -0700

    Rm unneeded vite configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,5 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
-  vite: {
-    server: {
-      host: "0.0.0.0",
-      hmr: { clientPort: 3000 },
-      port: 3000,
-      watch: { usePolling: true },
-    },
-  },
+  integrations: [react(), tailwind()],
 });
