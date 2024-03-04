05bffe17f5292ff95a4014b0dba1ae50f20cdf74
Ronald Libago <me@ronaldlibago.dev>
2024-03-04 10:21:26 -0800

    Add HTML util

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -13,3 +13,18 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+/**
+ * Accepts a string. Returns the string.
+ *
+ * Why? Because using this as a tag template will signal to Prettier to format
+ * the content as HTML.
+ *
+ * @example html`<strong>Hello world!</strong>`
+ *
+ * @param content - An HTML string.
+ * @returns The HTML string.
+ */
+export function html(content: TemplateStringsArray) {
+  return content;
+}
