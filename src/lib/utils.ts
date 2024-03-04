89d6a3a5e0d9f1765078f369da27ba725427200a
Ronald Libago <36778342+Mister-Corn@users.noreply.github.com>
2024-03-04 10:33:00 -0800

    Add projects section to web page (#2)
    * Bonus: Add doc string to cn function
    
    * Add HTML util
    
    * Setup new lucide icons in Layout
    
    * Add Projects section to web page
    
    * Bonus: Use optimized img for bio pic
    
    * Bonus: Add missing alt attrs to decorative imgs

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,30 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+/**
+ * Combines multiple class names into a single string.
+ *
+ * If combining Tailwind classes, 'tailwind-merge' is used to resolve conflicting
+ * classes.
+ *
+ * @param inputs - The class names to be combined.
+ * @returns The combined class names as a string.
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+/**
+ * Accepts a string. Returns the string.
+ *
+ * Why? Because using this as a tag template will signal to Prettier to format
+ * the content as HTML.
+ *
+ * @example html`<strong>Hello world!</strong>`
+ *
+ * @param content - An HTML string.
+ * @returns The HTML string.
+ */
+export function html(content: TemplateStringsArray) {
+  return content;
 }
