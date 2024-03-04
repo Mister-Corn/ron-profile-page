788a14055bc0ad7716b5cda347a7f7c34e7c42b9
Ronald Libago <me@ronaldlibago.dev>
2024-03-04 08:42:05 -0800

    Bonus: Add doc string to cn function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,15 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+/**
+ * Combines multiple class names into a single string.
+ *
+ * If combining Tailwind classes, 'tailwind-merge' is used to resolve conflicting
+ * classes.
+ *
+ * @param inputs - The class names to be combined.
+ * @returns The combined class names as a string.
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
